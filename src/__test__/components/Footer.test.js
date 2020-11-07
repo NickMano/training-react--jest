0c65665718fdee428a0e392a266faab030862d6f
Nico <nmanograsso@gmail.com>
2020-11-06 23:58:43 -0300

    Add snapshot test to header and footer

--- src/__test__/components/Footer.test.js
+++ src/__test__/components/Footer.test.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import {mount} from 'enzyme'
 import Footer from '../../components/Footer'
+import {create} from 'react-test-renderer'
 
-describe('Testing Footer component', () => {
+describe('<Footer />', () => {
     const footer = mount(<Footer/>)
     test('Rendering Footer component', () => {
         expect(footer.length).toEqual(1)
     })  
-    test('', () => {
+    test('Rendering footer title', () => {
         expect(footer.find(".Footer-title").text()).toEqual("Platzi Store")
+    });
+});
+
+describe('Footer Snapshot', () => {
+    test('Testing Snapshot of footer', () => {
+        const footer = create(<Footer/>)
+        expect(footer.toJSON()).toMatchSnapshot()
+        
     });
     
 });
--- src/__test__/components/Header.test.js
+++ src/__test__/components/Header.test.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {mount, shallow} from 'enzyme'
 import ProviderMock from '../../__mocks__/ProviderMock'
+import {create} from 'react-test-renderer'
 import Header from '../../components/Header'
 
 describe('<Header />', () => {
@@ -19,6 +20,18 @@ describe('<Header />', () => {
             </ProviderMock>
         )
         expect(header.find(".Header-title").text()).toEqual('Platzi Store')
+    });    
+});
+
+describe('Header Snapshot', () => {
+    test('Testing snapshot of header', () => {
+        const header = create(
+            <ProviderMock>
+                <Header />
+            </ProviderMock>
+        )
+        expect(header.toJSON()).toMatchSnapshot()
     });
     
 });
+
